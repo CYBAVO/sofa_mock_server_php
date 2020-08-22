e0d257365d5937bc58d2027d1ca7eb9a0ade642d
JoeLee <joe@cybavo.com>
2020-08-23 00:23:25 +0800

    Fix undefined variable warning

--- helper/randstr.php
+++ helper/randstr.php
@@ -12,7 +12,7 @@
 function random_string($length = 8) {
     $charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     $charsetLen = strlen($charset);
-    $randomString = '';
+    $r = '';
     for ($i = 0; $i < $length; $i++) {
         $r .= $charset[rand(0, $charsetLen - 1)];
     }
