b8925285d4d635e368efb11a1205962d7e8d5d4a
JoeLee <joe@cybavo.com>
2020-10-24 19:38:34 +0800

    Add 'Vault Wallet Balance' query API

--- index.php
+++ index.php
@@ -194,6 +194,12 @@ if (preg_match('/\/v1\/mock\/wallets\/(?<wallet_id>\d+)\/apitoken$/i', $path, $m
     log_access($uri, $resp);
     echo response($resp);
     return;
+} else if (preg_match('/\/v1\/mock\/wallets\/(?<wallet_id>\d+)\/vault\/balance$/i', $path, $m)) {
+    $uri = '/v1/sofa/wallets/'.$m['wallet_id'].'/vault/balance';
+    $resp = make_request($m['wallet_id'], $method, $uri, $query, $post_data);
+    log_access($uri, $resp);
+    echo response($resp);
+    return;
 } else if (!strcmp($path, '/v1/mock/wallets/callback')) {
     $callback = json_decode($post_data, true);
     $ac = get_api_code($callback['wallet_id']);
