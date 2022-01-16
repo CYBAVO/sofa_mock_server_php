0390b045ab8a06a4f351b21bdee0ca7c45b190f8
JoeLee <joe@cybavo.com>
2022-01-16 17:43:52 +0800

    Add API for the deposit addresses verification

--- index.php
+++ index.php
@@ -482,6 +482,12 @@ if (preg_match('/\/v1\/mock\/wallets\/(?<wallet_id>\d+)\/apitoken$/i', $path, $m
     log_access($uri, $resp);
     echo response($resp);
     return;
+} else if (preg_match('/\/v1\/mock\/wallets\/(?<wallet_id>\d+)\/receiver\/addresses\/verify$/i', $path, $m)) {
+    $uri = '/v1/sofa/wallets/'.$m['wallet_id'].'/receiver/addresses/verify';
+    $resp = make_request($m['wallet_id'], $method, $uri, $query, $post_data);
+    log_access($uri, $resp);
+    echo response($resp);
+    return;
 }
 
 $resp['status'] = 404;
