c34c796b5ea95eb7feb4951117df0ec6510e313a
JoeLee <joe@cybavo.com>
2022-06-15 10:01:16 +0800

    Update sample code for NFT meta information query API

--- index.php
+++ index.php
@@ -500,6 +500,12 @@ if (preg_match('/\/v1\/mock\/wallets\/(?<wallet_id>\d+)\/apitoken$/i', $path, $m
     log_access($uri, $resp);
     echo response($resp);
     return;
+} else if (preg_match('/\/v1\/mock\/currency\/(?<currency>\d+)\/contract\/get-multiple-tokenuri$/i', $path, $m)) {
+    $uri = '/v1/sofa/currency/'.$m['currency'].'/contract/get-multiple-tokenuri';
+    $resp = make_request($m['wallet_id'], $method, $uri, $query, $post_data);
+    log_access($uri, $resp);
+    echo response($resp);
+    return;
 }
 
 $resp['status'] = 404;
